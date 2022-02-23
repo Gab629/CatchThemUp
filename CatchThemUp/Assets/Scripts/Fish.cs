c38f018366e53ea7db2dcf52b6688de2d1a84e1b
Gab629 <70355153+Gab629@users.noreply.github.com>
2022-02-23 13:33:48 -0500

    fish working

--- CatchThemUp/Assets/Scripts/Fish.cs
+++ CatchThemUp/Assets/Scripts/Fish.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     public int scorePoisson;
-    public float speed;
+    public float speed = 2f;
     public int health;
 
+
+
+    //Mouvement poisson
     
+    public Transform target;
+    private Vector3 targetPos;
+    private Vector3 thisPos;
+    private float angle;
+    private float rotationOffset = 90;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Target").GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
@@ -24,7 +34,14 @@ public class Fish : MonoBehaviour
             Destroy(gameObject);
         }
 
+        gameObject.transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
+        targetPos = target.position;
+        thisPos = transform.position;
+        targetPos.x = targetPos.x - thisPos.x;
+        targetPos.y = targetPos.y - thisPos.y;
+        angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + rotationOffset));
     }
 
 }
--- CatchThemUp/Assets/Scripts/FishingMecanic.cs
+++ CatchThemUp/Assets/Scripts/FishingMecanic.cs
@@ -9,35 +9,12 @@ public class FishingMecanic : MonoBehaviour
     public Camera cam;
     public GameObject gameManager;
 
-    //Mouvement poisson
-    public float speed = 2f;
-    public Transform target;
-
-    private Vector3 targetPos;
-    private Vector3 thisPos;
-    private float angle;
-    private float rotationOffset = 90;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-         target = GameObject.FindGameObjectWithTag("Target").GetComponent<Transform>();
-
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        
-        targetPos = target.position;
-        thisPos = transform.position;
-        targetPos.x = targetPos.x - thisPos.x;
-        targetPos.y = targetPos.y - thisPos.y;
-        angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + rotationOffset));
-
+ 
             if (Input.GetMouseButtonDown(0))
             {
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
